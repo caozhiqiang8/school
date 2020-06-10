0f424138aafbd346ee6a082930a78716be40924f
czq411127520 <411127520@qq.com>
2020-06-10 18:39:26 +0800

    'user'

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.conf.urls import url
-from user.views import userIndex
+from user.views import userIndex,userTea,userStu
 
 
 urlpatterns = [
-    path('',userIndex.as_view(),name = 'user')
+    path('',userIndex.as_view(),name = 'user'),
+    path('teacher/',userTea.as_view(),name = 'teacher'),
+    path('student/',userStu.as_view(),name = 'student'),
 
 ]
